a54a1e170aa69006c7cb19a2fd854735f50bd1f2
Christine Paine <42606534+paineac@users.noreply.github.com>
2018-09-23 13:04:51 +0100

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import DishDetail from './DishDetailComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
@@ -40,21 +41,22 @@ class Main extends Component {
               <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
                 comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
           );
-        };
+        }
 
       return(
           <div>
             <Header />
             <Switch>
                  <Route path='/home' component={HomePage} />
+                 <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
                  <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                  <Route path='/menu/:dishId' component={DishWithId} />
-                 <Route exact path='/contactus' component={Contact} />} />
+                 <Route exact path='/contactus' component={Contact} />
                  <Redirect to="/home" />
              </Switch>
              <Footer />
             </div>
-        )
+        );
     }
 }
 export default Main;
