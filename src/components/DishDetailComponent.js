1f48a416688c62e1895d7b856a7fed7c0ae97f16
Christine Paine <42606534+paineac@users.noreply.github.com>
2018-09-30 09:35:50 +0100

    Redux Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody,
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -149,7 +150,25 @@ render() {
             }
 
         const  DishDetail = (props) => {
-            if(props.dish != null) {
+            if (props.isLoading) {
+           return(
+               <div className="container">
+                   <div className="row">
+                       <Loading />
+                   </div>
+               </div>
+           );
+       }
+       else if (props.errMess) {
+           return(
+               <div className="container">
+                   <div className="row">
+                       <h4>{props.errMess}</h4>
+                   </div>
+               </div>
+           );
+       }
+       else if (props.dish != null)
                 return (
                        <div className="container">
                        <div className="row">
@@ -174,11 +193,6 @@ render() {
                            </div>
                        </div>
                          </div>
-          )}
-              else {
-                  return (
-                      (<div></div>)
-                        )
-                  }
+          )
           }
      export default DishDetail;
